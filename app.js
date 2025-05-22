dedc36a853b1b2ba0a0e7950ad47ef79f45aeb57
santehventilru <ventilsantehjob@mail.ru>
2025-05-22 16:34:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -176,5 +176,5 @@ app.get('*', (req, res) => {
   }); */
 
 
-  const PORT = process.env.PORT;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, '0.0.0.0',  () => console.log(`Server running on port ${PORT}`));
